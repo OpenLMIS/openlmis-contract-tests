a9ac597888198428aa6b3de7be0e80380b9fde66
Paweł Gesek <pgesek@soldevelo.com>
2017-02-20 16:39:48 +0100

    Log service output every time

--- run_contract_tests.sh
+++ run_contract_tests.sh
@@ -38,12 +38,10 @@ sed -e "s/MAIL_PASSWORD=/$mail/g" -i .env
 #cleaning after tests
 contract_test_result=$?
 
-if [ $contract_test_result -ne 0 ]; then
-  echo "========== Logging output from containers =========="
-  /usr/local/bin/docker-compose logs
-  echo "========== Logging nginx settings =========="
-  /usr/local/bin/docker-compose exec nginx cat /etc/nginx/conf.d/default.conf
-fi
+echo "========== Logging output from containers =========="
+/usr/local/bin/docker-compose logs
+echo "========== Logging nginx settings =========="
+/usr/local/bin/docker-compose exec nginx cat /etc/nginx/conf.d/default.conf
 
 /usr/local/bin/docker-compose -f docker-compose.yml -f ${FILENAME} down $2
 #don't remove the $1 in the line above
