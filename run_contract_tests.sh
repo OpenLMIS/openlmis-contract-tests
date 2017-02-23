a63a4682c19cb7dc72d1e00b40278380ccaa7783
Chongsun Ahn <cahn922@yahoo.com>
2017-02-22 22:11:24 -0800

    OLMIS-1933 Switch from using public env file
    Instead of curling the public env file and replacing mail settings (including password!) before running the contract tests, we should have CI have a private env file in each contract test job.

--- run_contract_tests.sh
+++ run_contract_tests.sh
@@ -2,35 +2,8 @@
 
 FILENAME=${1}
 
-#download env file
-curl -LO https://raw.githubusercontent.com/OpenLMIS/openlmis-config/master/.env
-
 #pull all images
 /usr/local/bin/docker-compose -f docker-compose.yml -f ${FILENAME} pull
-
-#change BASE_URL host value from localhost to nginx
-ip="BASE_URL=http:\/\/nginx"
-sed -e "s/BASE_URL=http:\/\/localhost/$ip/g" -i .env
-
-#change CONSUL_HOST value from localhost to consul
-ip="CONSUL_HOST=consul"
-sed -e "s/CONSUL_HOST=localhost/$ip/g" -i .env
-
-#change MAIL_HOST value from localhost to smtp.gmail.com
-mail="MAIL_HOST=smtp.gmail.com"
-sed -e "s/MAIL_HOST=localhost/$mail/g" -i .env
-
-#change MAIL_PORT value from 25 to 465
-mail="MAIL_PORT=465"
-sed -e "s/MAIL_PORT=25/$mail/g" -i .env
-
-#change MAIL_USERNAME value from noreply@openlmis.org to contract.tests@gmail.com
-mail="MAIL_USERNAME=contract.tests@gmail.com"
-sed -e "s/MAIL_USERNAME=noreply@openlmis.org/$mail/g" -i .env
-
-#change MAIL_PASSWORD value from "" to olmis1234
-mail="MAIL_PASSWORD=olmis1234"
-sed -e "s/MAIL_PASSWORD=/$mail/g" -i .env
 
 #run docker file
 /usr/local/bin/docker-compose -f docker-compose.yml -f ${FILENAME} run contract_tests
@@ -44,10 +17,9 @@ echo "========== Logging nginx settings =========="
 /usr/local/bin/docker-compose exec nginx cat /etc/nginx/conf.d/default.conf
 
 /usr/local/bin/docker-compose -f docker-compose.yml -f ${FILENAME} down $2
-#don't remove the $1 in the line above
+#don't remove the $2 in the line above
 #CI will append -v to it, so all dangling volumes are removed after the job runs
 
 exit ${contract_test_result}
 #this line above makes sure when jenkins runs this script
-
 #the build success/failure result is determined by contract test run not by the clean up run
