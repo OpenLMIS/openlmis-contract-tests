16f23823f514b308e187f1295b01c7c983e1ab0a
Yi Deng <dydyistc@gmail.com>
2017-03-06 15:13:07 +0800

    OLMIS-1440 as administrator user should be able to assign reason to program and facility type

--- src/cucumber/java/org/openlmis/contract_tests/stockmanagement/StockReasonsStepDefs.java
+++ src/cucumber/java/org/openlmis/contract_tests/stockmanagement/StockReasonsStepDefs.java
@@ -1,26 +1,44 @@
 package org.openlmis.contract_tests.stockmanagement;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.path.json.JsonPath.from;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
 import static org.openlmis.contract_tests.common.LoginStepDefs.ACCESS_TOKEN;
 import static org.openlmis.contract_tests.common.TestVariableReader.baseUrlOfService;
 
 import org.apache.http.HttpStatus;
+import org.json.simple.JSONObject;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
+import java.util.ArrayList;
+
 public class StockReasonsStepDefs {
   private static final String URL_OF_STOCK_REASON_CATEGORIES =
-      baseUrlOfService("stockmanagement") + "reasonCategories";
+      baseUrlOfService("stockmanagement") + "reasonCategories/";
   private static final String URL_OF_STOCK_REASON_TYPES =
-      baseUrlOfService("stockmanagement") + "reasonTypes";
+      baseUrlOfService("stockmanagement") + "reasonTypes/";
+  private static final String URL_OF_STOCK_REASONS =
+      baseUrlOfService("stockmanagement") + "stockCardLineItemReasons/";
+  private static final String URL_OF_VALID_REASON_ASSIGNMENTS =
+      baseUrlOfService("stockmanagement") + "validReasons/";
 
   private static final String ACCESS_TOKEN_PARAM_NAME = "access_token";
+  private static final String PROGRAM_PARAM_NAME = "program";
+  private static final String FACILITY_TYPE_PARAM_NAME = "facilityType";
+
+  private static final String PROGRAM = "dce17f2e-af3e-40ad-8e00-3496adef44c3";
+  private static final String FACILITY_TYPE = "663b1d34-cc17-4d60-9619-e553e45aa441";
 
   private Response reasonResponse;
+  private String createdReasonId;
+  private String validReasonAssignmentId;
 
   @When("^I try to get stock card line item reason types$")
   public void iTryToGetStockCardLineItemReasonTypes() throws Throwable {
@@ -54,5 +72,115 @@ public class StockReasonsStepDefs {
     reasonResponse.then()
         .statusCode(HttpStatus.SC_OK)
         .body("$", hasItems("AD_HOC", "ADJUSTMENT"));
+  }
+
+  @When("^I try to create a new stock card line item reason$")
+  public void iTryToCreateANewStockCardLineItemReason(String bodyString) throws Throwable {
+    reasonResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .body(bodyString)
+        .when()
+        .post(URL_OF_STOCK_REASONS);
+  }
+
+  @Then("^I should get response of reason created$")
+  public void iShouldGetResponseOfReasonCreated() throws Throwable {
+    reasonResponse.then().statusCode(HttpStatus.SC_CREATED);
+    createdReasonId = from(reasonResponse.asString()).get("id");
+  }
+
+  @When("^I try to get all stock card line item reasons$")
+  public void iTryToGetAllStockCardLineItemReasons() throws Throwable {
+    reasonResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .when()
+        .get(URL_OF_STOCK_REASONS);
+  }
+
+  @Then("^I should get response of all reasons that contains created reason$")
+  public void iShouldGetResponseOfAllReasonsThatContainsCreatedReason() throws Throwable {
+    reasonResponse.then()
+        .statusCode(HttpStatus.SC_OK)
+        .body("id", hasItem(createdReasonId));
+  }
+
+  @When("^I try to update a stock card line item reason$")
+  public void iTryToUpdateAStockCardLineItemReason(String bodyString) throws Throwable {
+    reasonResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .body(bodyString)
+        .when()
+        .put(URL_OF_STOCK_REASONS + createdReasonId);
+  }
+
+  @Then("^I should get response of all reasons that contains updated reason name (.*)$")
+  public void iShouldGetResponseOfAllReasonsThatContainsUpdatedReasonName(String reasonName)
+      throws Throwable {
+    reasonResponse.then()
+        .statusCode(HttpStatus.SC_OK)
+        .body("name", hasItem(reasonName));
+  }
+
+  @When("^I try to assign created reason to program and facility type combination$")
+  public void iTryToAssignCreatedReasonToProgramAndFacilityTypeCombination() throws Throwable {
+    reasonResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .body(createValidReasonJson())
+        .when()
+        .post(URL_OF_VALID_REASON_ASSIGNMENTS);
+    reasonResponse.then().statusCode(HttpStatus.SC_CREATED);
+  }
+
+  @And("^I try to get all valid reason assignments$")
+  public void iTryToGetAllValidReasonAssignments() throws Throwable {
+    reasonResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .queryParam(PROGRAM_PARAM_NAME, PROGRAM)
+        .queryParam(FACILITY_TYPE_PARAM_NAME, FACILITY_TYPE)
+        .when()
+        .get(URL_OF_VALID_REASON_ASSIGNMENTS);
+  }
+
+  @Then("^I should get response of all valid reason assignments that contains newly assignment$")
+  public void iShouldGetResponseOfAllValidReasonAssignmentsThatContainsNewlyAssignment()
+      throws Throwable {
+    ArrayList<String> assignmentIds = from(reasonResponse.asString()).get("id");
+    validReasonAssignmentId = assignmentIds.get(assignmentIds.size() - 1);
+    reasonResponse.then()
+        .statusCode(HttpStatus.SC_OK)
+        .body("reason.id", hasItem(createdReasonId));
+  }
+
+
+  @When("^I try to detach created reason to program and facility type combination$")
+  public void iTryToDetachCreatedReasonToProgramAndFacilityTypeCombination() throws Throwable {
+    reasonResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .when()
+        .delete(URL_OF_VALID_REASON_ASSIGNMENTS + validReasonAssignmentId);
+    reasonResponse.then().statusCode(HttpStatus.SC_NO_CONTENT);
+  }
+
+  @Then("^I should get response of all valid reason assignments that not contains detached assignment$")
+  public void iShouldGetResponseOfAllValidReasonAssignmentsThatNotContainsDetachedAssignment() throws Throwable {
+    reasonResponse.then()
+        .statusCode(HttpStatus.SC_OK)
+        .body("id", not(hasItem(validReasonAssignmentId)));
+  }
+
+  private JSONObject createValidReasonJson() {
+    JSONObject json = new JSONObject();
+    json.put("programId", PROGRAM);
+    json.put("facilityTypeId", FACILITY_TYPE);
+    JSONObject reason = new JSONObject();
+    reason.put("id", createdReasonId);
+    json.put("reason", reason);
+    return json;
   }
 }
--- src/cucumber/resources/org/openlmis/contract_tests/stockmanagement_tests/StockReasonsTests.feature
+++ src/cucumber/resources/org/openlmis/contract_tests/stockmanagement_tests/StockReasonsTests.feature
@@ -1,7 +1,7 @@
 @StockReasonsTests
 Feature: Stock Reasons Tests
 
-  Scenario: Administrator user should be able to configure stock card line item reasons
+  Scenario: Administrator user should be able to get available reason types and reason categories
     Given I have logged in as administrator
 
     When I try to get stock card line item reason types
@@ -9,3 +9,47 @@ Feature: Stock Reasons Tests
 
     When I try to get stock card line item reason categories
     Then I should get response of stock card line item reason categories
+
+    And I logout
+
+  Scenario: Administrator user should be able to assign reasons to program and facility type
+    Given I have logged in as administrator
+
+    When I try to create a new stock card line item reason
+    """
+    {
+      "name": "test_reason_1",
+      "isFreeTextAllowed": true,
+      "reasonCategory": "AD_HOC",
+      "reasonType": "CREDIT",
+      "description": "test description"
+    }
+    """
+    Then I should get response of reason created
+
+    When I try to get all stock card line item reasons
+    Then I should get response of all reasons that contains created reason
+
+    When I try to update a stock card line item reason
+    """
+    {
+      "name": "update_reason_1",
+      "isFreeTextAllowed": true,
+      "reasonCategory": "AD_HOC",
+      "reasonType": "CREDIT",
+      "description": "test description"
+    }
+    """
+    And I try to get all stock card line item reasons
+    Then I should get response of all reasons that contains updated reason name update_reason_1
+
+    When I try to assign created reason to program and facility type combination
+    And I try to get all valid reason assignments
+    Then I should get response of all valid reason assignments that contains newly assignment
+
+    When I try to detach created reason to program and facility type combination
+    And I try to get all valid reason assignments
+    Then I should get response of all valid reason assignments that not contains detached assignment
+
+    And I logout
+
