9f15e1ad260d48b4e9f7cddf9b09c8e12d78ec0a
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-02-21 09:21:20 +0100

    OLMIS-235: Added fulfillment tests

---
+++ src/cucumber/java/org/openlmis/contract_tests/fulfillment/FulfillmentStepDefs.java
@@ -0,0 +1,330 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+package org.openlmis.contract_tests.fulfillment;
+
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
+import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.openlmis.contract_tests.common.LoginStepDefs.ACCESS_TOKEN;
+import static org.openlmis.contract_tests.common.TestVariableReader.baseUrlOfService;
+
+import com.google.gson.JsonObject;
+
+import org.apache.http.HttpStatus;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+
+import cucumber.api.DataTable;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+public class FulfillmentStepDefs {
+
+  private Response shipmentResponse;
+  private Response orderResponse;
+  private Response requisitionResponse;
+  private Response stockCardResponse;
+  private Response proofOfDeliveryResponse;
+
+  private String orderId;
+  private String shipmentId;
+  private String stockCardId;
+  private String proofOfDeliveryId;
+
+  private int stockOnHand;
+
+  private static final String URL_OF_SHIPMENTS =
+      baseUrlOfService("fulfillment") + "shipments";
+  private static final String URL_OF_ORDERS =
+      baseUrlOfService("fulfillment") + "orders";
+  private static final String URL_OF_PODS =
+      baseUrlOfService("fulfillment") + "proofsOfDelivery";
+  private static final String URL_OF_REQUISITION_CONVERT =
+      baseUrlOfService("requisition") + "requisitions/convertToOrder";
+  private static final String URL_OF_STOCK_CARD_SUMMARIES =
+      baseUrlOfService("stockmanagement") + "stockCardSummaries";
+  private static final String URL_OF_STOCK_CARD_MANAGEMENT =
+      baseUrlOfService("stockmanagement") + "stockCards/";
+
+  private static final String CONTENT_ID = "content[0].id";
+  private static final String STOCK_ON_HAND = "content[0].stockOnHand";
+  private static final String ACCESS_TOKEN_PARAM_NAME = "access_token";
+
+  static {
+    enableLoggingOfRequestAndResponseIfValidationFails();
+  }
+
+  @Then("^I have got stock card id for$")
+  public void gotStockCardId(DataTable table) {
+    Map<String, String> data = table.asMaps(String.class, String.class).get(0);
+
+    RequestSpecification request = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN);
+
+    data.forEach(request::queryParam);
+
+    stockCardResponse = request
+        .when()
+        .get(URL_OF_STOCK_CARD_SUMMARIES).andReturn();
+
+    stockCardId = stockCardResponse.jsonPath().getString(CONTENT_ID);
+    stockOnHand = stockCardResponse.jsonPath().getInt(STOCK_ON_HAND);
+  }
+
+  @When("^I try to get stock card with id$")
+  public void tryToGetStockCardWithId() {
+    stockCardResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .when()
+        .get(URL_OF_STOCK_CARD_MANAGEMENT + stockCardId);
+  }
+
+  @Then("^I should get a stock card$")
+  public void shouldGetAStockCardWithStockOnHand() {
+    stockCardResponse
+        .then()
+        .statusCode(HttpStatus.SC_OK)
+        .body(STOCK_ON_HAND, is(stockOnHand));
+  }
+
+  @Then("^I should get a stock card with zero stock on hand$")
+  public void shouldGetStockCardWithStockOnHandSubstracted() {
+    stockCardResponse
+        .then()
+        .statusCode(HttpStatus.SC_OK)
+        .body(STOCK_ON_HAND, is(0));
+  }
+
+  @When("^I try to convert requisition with:$")
+  public void tryToConvertRequisition(DataTable table) {
+    requisitionResponse = given().contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .body(createBodyForConvertToOrder(table))
+        .when()
+        .post(URL_OF_REQUISITION_CONVERT);
+  }
+
+  @Then("^I should get response of order created$")
+  public void shouldGetResponseOfOrderCreated() {
+    requisitionResponse.then()
+        .statusCode(SC_CREATED);
+  }
+
+  @When("^I try to get order by:$")
+  public void tryToGetOrderBy(DataTable table) {
+    Map<String, String> data = table.asMaps(String.class, String.class).get(0);
+
+    RequestSpecification request = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN);
+    
+    data.forEach(request::queryParam);
+    
+    orderResponse = request
+        .when()
+        .get(URL_OF_ORDERS)
+        .andReturn();
+  }
+
+  @Then("^I should get response of order found")
+  public void shouldGetResponseOfOrderFound() {
+    orderResponse.then()
+        .body(CONTENT_ID, notNullValue())
+        .statusCode(SC_OK);
+
+    orderId = orderResponse.jsonPath().getString(CONTENT_ID);
+  }
+  
+  @When("^I try to get shipment by order id$")
+  public void tryToGetShipmentByOrderId() {
+    shipmentResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .queryParam("orderId", orderId)
+        .when()
+        .get(URL_OF_SHIPMENTS)
+        .andReturn();
+  }
+  
+  @Then("^I should get response of shipment found$")
+  public void shouldGetResponseOfShipmentFound() {
+    shipmentResponse
+        .then()
+        .body(CONTENT_ID, notNullValue())
+        .statusCode(SC_OK);
+
+    shipmentId = shipmentResponse.jsonPath().getString(CONTENT_ID);
+  }
+
+  @Then("^I should get response of shipment not found$")
+  public void shouldGetResponseOfShipmentNotFound() {
+    shipmentResponse
+        .then()
+        .body("content.isEmpty()", is(true))
+        .statusCode(SC_OK);
+  }
+
+  @When("^I try to get proof of delivery by shipment id$")
+  public void tryToGetProofOfDeliveryByShipmentId() {
+    proofOfDeliveryResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .queryParam("shipmentId", shipmentId)
+        .when()
+        .get(URL_OF_PODS)
+        .andReturn();
+  }
+
+  @Then("^I should get response of proof of delivery found$")
+  public void shouldGetResponseOfProofOfDeliveryFound() {
+    proofOfDeliveryResponse.then()
+        .body(CONTENT_ID, notNullValue())
+        .statusCode(SC_OK);
+
+    proofOfDeliveryId = proofOfDeliveryResponse.jsonPath().getString(CONTENT_ID);
+  }
+
+  @When("^I try to confirm the proof of delivery$")
+  public void tryToConfirmProofOfDelivery(DataTable table) throws ParseException {
+    JSONParser parser = new JSONParser();
+    JSONObject page = (JSONObject) parser.parse(proofOfDeliveryResponse.asString());
+    JSONArray content = (JSONArray) page.get("content");
+
+    JSONObject body = (JSONObject) content.get(0);
+    body.put("status", "CONFIRMED");
+    body.put("receivedBy", "Someone");
+    body.put("deliveredBy", "Someone");
+    body.put("receivedDate", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+    List<Map<String, String>> data = table.asMaps(String.class, String.class);
+    JSONArray lineItems = (JSONArray) body.get("lineItems");
+
+    for (Map<String, String> map : data) {
+      for (Object line : lineItems) {
+        JSONObject obj = (JSONObject) line;
+        JSONObject orderable = (JSONObject) obj.get("orderable");
+        JSONObject lot = (JSONObject) obj.get("lot");
+
+        boolean orderableMatch = map.get("orderableId").equals(orderable.get("id"));
+        boolean lotMatch = null == lot || map.get("lotId").equals(lot.get("id"));
+
+        if (orderableMatch && lotMatch) {
+          obj.put("quantityAccepted", map.get("quantityAccepted"));
+          obj.put("quantityRejected", 0);
+
+          if ("true".equals(obj.get("useVvm").toString())) {
+            obj.put("vvmStatus", "STAGE_2");
+          }
+        }
+      }
+    }
+
+    proofOfDeliveryResponse = given()
+        .contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .body(body)
+        .when()
+        .put(URL_OF_PODS + '/' + proofOfDeliveryId)
+        .andReturn();
+  }
+
+  @Then("^I should get response of proof of delivery confirmation success$")
+  public void shouldGetResponseOfProofOfDeliveryConfirmationSuccess() throws ParseException {
+    proofOfDeliveryResponse
+        .then()
+        .statusCode(SC_OK);
+
+    JSONParser parser = new JSONParser();
+    JSONObject body = (JSONObject) parser.parse(proofOfDeliveryResponse.asString());
+    assertThat(body.get("id").toString(), is(proofOfDeliveryId));
+    assertThat(body.get("status").toString(), is("CONFIRMED"));
+  }
+
+  @When("^I try to finalize shipment$")
+  public void tryToFinalizeShipment(DataTable table) {
+    List<Map<String, String>> data = table.asMaps(String.class, String.class);
+    JSONArray lineItems = new JSONArray();
+
+    for (Map map : data) {
+      JSONObject json = new JSONObject();
+      json.put("orderable", createObjectReference(map.get("orderableId")));
+
+      if (map.containsKey("lotId")) {
+        json.put("lot", createObjectReference(map.get("lotId")));
+      }
+
+      json.put("quantityShipped", map.get("quantityShipped"));
+
+      lineItems.add(json);
+    }
+
+    JSONObject body = new JSONObject();
+    body.put("order", createObjectReference(orderId));
+    body.put("notes", "some notes");
+    body.put("lineItems", lineItems);
+
+    shipmentResponse = given().contentType(ContentType.JSON)
+        .queryParam(ACCESS_TOKEN_PARAM_NAME, ACCESS_TOKEN)
+        .body(body)
+        .when()
+        .post(URL_OF_SHIPMENTS);
+  }
+
+  @Then("^I should get response of shipment created$")
+  public void shouldGetResponseOfShipmentCreated() {
+    shipmentResponse.then()
+        .statusCode(SC_CREATED);
+  }
+
+  private JSONObject createObjectReference(Object id) {
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("id", id);
+    return jsonObject;
+  }
+
+  private JSONArray createBodyForConvertToOrder(DataTable table) {
+    List<Map<String, String>> data = table.asMaps(String.class, String.class);
+    JSONArray array = new JSONArray();
+
+    for (Map map : data) {
+      JSONObject json = new JSONObject();
+      json.put("requisitionId", map.get("requisitionId"));
+      json.put("supplyingDepotId", map.get("supplyingDepotId"));
+
+      array.add(json);
+    }
+
+    return array;
+  }
+}
---
+++ src/cucumber/resources/org/openlmis/contract_tests/fulfillment_tests/FulfillmentTests.feature
@@ -0,0 +1,88 @@
+@FulfillmentTests
+Feature: Fulfillment Tests
+
+  Scenario: Administrator should be able to confirm Proof of Delivery (External Order Path)
+    Given I have logged in as administrator
+
+    When I try to convert requisition with:
+      | requisitionId                        | supplyingDepotId                     |
+      | 21d138bc-f2c2-4188-adf3-a3aae381abde | 19121381-9f3d-4e77-b9e5-d3f59fc1639e |
+    Then I should get response of order created
+
+    When I try to get order by:
+      | supplyingFacilityId                  | requestingFacilityId                 | programId                            | periodStartDate | periodEndDate |
+      | 19121381-9f3d-4e77-b9e5-d3f59fc1639e | 7938919f-6f61-4d1a-a4dc-923c31e9cd45 | dce17f2e-af3e-40ad-8e00-3496adef44c3 | 2017-07-01      | 2017-09-30    |
+    Then I should get response of order found
+
+    When I try to get shipment by order id
+    Then I should get response of shipment found
+
+    When I try to get proof of delivery by shipment id
+    Then I should get response of proof of delivery found
+
+    When I try to confirm the proof of delivery
+      | orderableId                          | lotId | quantityAccepted |
+      | 7b9b66e0-f008-4c38-93ec-7a5f4868cca9 |       | 0                |
+      | bfde33c3-83f1-4feb-a27c-842852f66b71 |       | 0                |
+    Then I should get response of proof of delivery confirmation success
+    And I logout
+
+  Scenario: Divo user should be able to confirm Proof of Delivery (Local Order Path)
+    Given I have logged in as divo1
+
+    When I have got stock card id for
+      | program                              | facility                             |
+      | 418bdc1d-c303-4bd0-b2d3-d8901150a983 | c62dea9b-6974-4101-ba39-b09914165967 |
+    And I try to get stock card with card id
+    Then I should get a stock card
+
+    When I try to convert requisition with:
+      | requisitionId                        | supplyingDepotId                     |
+      | c537e925-a518-4f5b-8aef-6f07fd9aa58c | c62dea9b-6974-4101-ba39-b09914165967 |
+    Then I should get response of order created
+
+    When I try to get order by:
+      | supplyingFacilityId                  | requestingFacilityId                 | programId                            | processingPeriodId                   |
+      | c62dea9b-6974-4101-ba39-b09914165967 | 7fc9bda8-ad8a-468d-8244-38e1918527d5 | 418bdc1d-c303-4bd0-b2d3-d8901150a983 | 516ac930-0d28-49f5-a178-64764e22b236 |
+    Then I should get response of order found
+
+    When I try to get shipment by order id
+    Then I should get response of shipment not found
+
+    When I try to finalize shipment
+      | orderableId                          | lotId                                | quantityShipped |
+      | 7c58f053-0ca5-46ad-8209-98cff0c79b42 |                                      | 31              |
+      | 71204042-558e-4c47-a602-7886451e5689 |                                      | 0               |
+      | 9c3bea84-487b-4e22-8759-3fe93f8201d9 | f08d3db1-9132-487c-ba58-15ea12302aee | 99              |
+      | 9c3bea84-487b-4e22-8759-3fe93f8201d9 | 9d0cd7cb-5739-491b-8d6b-890fd17ec810 | 38              |
+      | 047638fa-92ce-4adf-9bbd-3bcb3216897a | f9f9bb7c-7636-40c1-a548-b45e918a512a | 557             |
+      | 047638fa-92ce-4adf-9bbd-3bcb3216897a | 06f86552-1aa9-47d6-8857-6b7ad824f805 | 180             |
+      | 047638fa-92ce-4adf-9bbd-3bcb3216897a |                                      | 107             |
+      | a852f785-d82e-4afe-bc6a-04f3236caf90 |                                      | 26              |
+      | 18223845-4d19-4408-b7c2-971c26014058 |                                      | 531             |
+      | b75a88c7-70fa-4bab-9785-3dd980a5ffc3 |                                      | 47              |
+    Then I should get response of shipment created
+
+    When I have got stock card id for
+      | program                              | facility                             |
+      | 418bdc1d-c303-4bd0-b2d3-d8901150a983 | c62dea9b-6974-4101-ba39-b09914165967 |
+    And I try to get stock card with card id
+    Then I should get a stock card with zero stock on hand
+
+    When I try to get proof of delivery by shipment id
+    Then I should get response of proof of delivery found
+
+    When I try to confirm the proof of delivery
+      | orderableId                          | lotId                                | quantityAccepted |
+      | 7c58f053-0ca5-46ad-8209-98cff0c79b42 |                                      | 31               |
+      | 71204042-558e-4c47-a602-7886451e5689 |                                      | 0                |
+      | 9c3bea84-487b-4e22-8759-3fe93f8201d9 | f08d3db1-9132-487c-ba58-15ea12302aee | 99               |
+      | 9c3bea84-487b-4e22-8759-3fe93f8201d9 | 9d0cd7cb-5739-491b-8d6b-890fd17ec810 | 38               |
+      | 047638fa-92ce-4adf-9bbd-3bcb3216897a | f9f9bb7c-7636-40c1-a548-b45e918a512a | 557              |
+      | 047638fa-92ce-4adf-9bbd-3bcb3216897a | 06f86552-1aa9-47d6-8857-6b7ad824f805 | 180              |
+      | 047638fa-92ce-4adf-9bbd-3bcb3216897a |                                      | 107              |
+      | a852f785-d82e-4afe-bc6a-04f3236caf90 |                                      | 26               |
+      | 18223845-4d19-4408-b7c2-971c26014058 |                                      | 531              |
+      | b75a88c7-70fa-4bab-9785-3dd980a5ffc3 |                                      | 47               |
+    Then I should get response of proof of delivery confirmation success
+    And I logout
